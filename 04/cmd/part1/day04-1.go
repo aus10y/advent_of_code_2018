d581c3e8c82f353a61e73e88f00ba6934f9af994
Austin Young <austiny88@gmail.com>
2018-12-07 08:21:02 -0600

    Solved day04-1.

--- 04/cmd/part1/day04-1.go
+++ 04/cmd/part1/day04-1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -24,9 +25,24 @@ type Record struct {
 	action Action
 }
 
+type recordSlice []Record
+
+func (p recordSlice) Len() int {
+	return len(p)
+}
+
+func (p recordSlice) Less(i, j int) bool {
+	a, b := p[i].time, p[j].time
+	return a.Before(b) || (a.Equal(b) && i < j)
+}
+
+func (p recordSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
 func parseRecord(line string) (record Record, err error) {
 	// Parse the Time:
-	// Ex. [1518-10-15 00:29] wakes up
+	// Ex. "[1518-10-15 00:29] wakes up"
 	t, err := time.Parse("2006-01-02 15:04", line[1:17])
 	if err != nil {
 		return record, err
@@ -34,7 +50,7 @@ func parseRecord(line string) (record Record, err error) {
 	record.time = t
 
 	// Parse the Guard ID, if present:
-	// Guard #503 begins shift
+	// Ex., ..." Guard #503 begins shift"
 	start := strings.Index(line, "#")
 	if start != -1 {
 		end := strings.Index(line, " begins")
@@ -58,23 +74,101 @@ func parseRecord(line string) (record Record, err error) {
 	return
 }
 
-func main() {
-	input, err := ioutil.ReadFile("../../input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lines := strings.Split(string(input), "\n")
-
-	records := make([]Record, len(lines))
+func parseRecords(lines []string) recordSlice {
+	records := make(recordSlice, len(lines))
 	for i, line := range lines {
 		record, err := parseRecord(line)
 		if err != nil {
 			log.Fatalf("%v\n", err)
 		}
 		records[i] = record
-		fmt.Printf("%v\n", record)
+	}
+	return records
+}
+
+type guardHabit struct {
+	asleep int
+	times  map[int]int
+}
+
+func main() {
+	var records recordSlice
+	{
+		input, err := ioutil.ReadFile("../../input.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lines := strings.Split(string(input), "\n")
+		records = parseRecords(lines)
 	}
 
-	// sort the records
+	sort.Sort(records)
+
+	// Strategy:
+	// Find the guard that has the most minutes asleep.
+	// What minute does that guard spend asleep the most?
+	//
+	// Walk the records, for each guard,
+	// Find their next period of sleep,
+	// Add the minutes asleep to that guard's cummulative sleep,
+	// Increment the guardHabit.times counter for each individual minute of sleep.
+	habits := make(map[int]guardHabit)
+
+	guard := 0
+	fellAsleep := 0
+	awoke := 0
+	for _, record := range records {
+		if record.guard != 0 {
+			guard = record.guard
+
+			// Records containing the guard ID are always the 'Begins' action.
+			continue
+		}
+
+		switch record.action {
+		case Sleeps:
+			fellAsleep = record.time.Minute()
+		case Wakes:
+			awoke = record.time.Minute()
+
+			for i := fellAsleep; i < awoke; i++ {
+				habit := habits[guard]
+				if habit.times == nil {
+					habit.times = make(map[int]int)
+				}
+				habit.asleep++
+				habit.times[i] = habit.times[i] + 1
+				habits[guard] = habit
+			}
+		default:
+			log.Fatalf("unanticipated action")
+		}
+	}
+
+	// Find the sleepiest guard.
+	sleepiest := 0
+	{
+		mostSleep := 0
+		for guard, habit := range habits {
+			if habit.asleep > mostSleep {
+				sleepiest = guard
+				mostSleep = habit.asleep
+			}
+		}
+	}
+
+	// Find the minute during which the guard is most often asleep.
+	minute := 0
+	{
+		sleep := 0
+		for m, s := range habits[sleepiest].times {
+			if s > sleep {
+				minute = m
+				sleep = s
+			}
+		}
+	}
+
+	fmt.Printf("Guard ID: %d, most often asleep during minute %d, Answer: %d\n", sleepiest, minute, sleepiest*minute)
 }
